6fe4d8aadc82f8e6514f3a1432aecb914bc24ae9
nniricha <73671693+nniricha@users.noreply.github.com>
2020-11-29 01:55:19 -0600

    Update Product and Services.cpp

--- Client Database System/Client Database System/Product and Services.cpp
+++ Client Database System/Client Database System/Product and Services.cpp
@@ -8,13 +8,74 @@
 
 #include "Product and Services.hpp"
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-void AddPrice(vector<int>& priceVector){
+Products_and_Services::Products_and_Services(){
+    product = "No product";
+    serviceDescription = "No service description";
+}
+
+void Product_and_Services::AddPrice(vector<int>& priceVector){
     int userPrice;
     
     cin >> userPrice;
     priceVector.push_back(userPrice);
+}
+
+void Product_and_Services::ReadFromFile(vector<string>& productVector, vector<string>& descriptionVector){
+}
+
+void Product_and_Services::ListServices(){
+}
+
+void Product_and_Services::AddNewService(string product, string serviceDescription, vector<string>& productVector, vector<string>& descriptionVector){
+    cout << "Input product, service description: ";
+    cin >> product;
+    cin >> serviceDescription;
+    
+    productVector.push_back(product);
+    descriptionVector.push_back(serviceDescription);
+}
+
+void Product_and_Services::UpdateService(string product, string serviceDescription, vector<string>& productVector, vector<string>& descriptionVector){
+    string userProduct;
+    string userService;
+    
+    cin >> userProduct;
+    cin >> userService;
+    
+    unsigned int i;
+    for(i = 0; i < productVector.size(); ++i){
+        if(userProduct == productVector.at(i)){
+            cout << "Input new product: " << endl;
+            cin >> userProduct;
+        }
+    }
+    for(i = 0; i < descriptionVector.size(); ++i){
+        if(userService == descriptionVector.at(i)){
+            cout << "Input new service: " << endl;
+            cin >> userService;
+        }
+    }
+}
+
+void Product_and_Services::PrintServicesReport(vector<string>& productVector, vector<string>& descriptionVector){
+    unsigned int i;
+    
+    for (i = 0; i < productVector.size(); ++i){
+        cout << productVector.at(i);
+    }
+    
+    for (i = 0; i < descriptionVector.size(); ++i){
+        cout << descriptionVector.at(i);
+    }
+}
+
+void Product_and_Services::SaveToFile(vector<string>& productVector, vector<string>& descriptionVector){
+
 }
